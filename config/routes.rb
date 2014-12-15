7964726fea31f2b1c399eb1c5f83117fd812d864
Helen Hood <helen.c.hood@gmail.com>
2014-12-15 15:55:02 -0500

    Add new meetup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'meetups#index'
+
+  # get "meetups/new" => "meetups#new"
+  resources :meetups, only: [:new]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
