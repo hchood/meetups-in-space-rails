534604078b7317e6ba0127f38e7999428af57487
Helen Hood <helen.c.hood@gmail.com>
2014-12-18 15:55:01 -0500

    Add login helper for feature specs

--- spec/features/user_adds_a_meetup_spec.rb
+++ spec/features/user_adds_a_meetup_spec.rb
@@ -19,12 +19,14 @@ feature "adds a meetup", %q{
   context "authenticated user" do
     before :each do
       @user = FactoryGirl.create(:user)
-      visit new_user_session_path
-      fill_in "Email", with: @user.email
-      fill_in "Password", with: @user.password
-      within ".new_user" do
-        click_on "Sign in"
-      end
+      login_as(@user)
+
+      # visit new_user_session_path
+      # fill_in "Email", with: @user.email
+      # fill_in "Password", with: @user.password
+      # within ".new_user" do
+      #   click_on "Sign in"
+      # end
     end
 
     scenario "with valid attributes" do
